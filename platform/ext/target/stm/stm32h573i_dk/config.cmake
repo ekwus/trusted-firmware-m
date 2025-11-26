99fdda781354991e577070a8c8b451696a57ccfa
David Ritchie <d.ritchie@skillseeder.com>
2025-11-26 12:09:05 +0000

    Enable EC256 and encrypted image

--- platform/ext/target/stm/stm32h573i_dk/config.cmake
+++ platform/ext/target/stm/stm32h573i_dk/config.cmake
@@ -15,8 +15,8 @@ set(MCUBOOT_UPGRADE_STRATEGY        "SWAP_USING_SCRATCH"      CACHE STRING    "U
 set(TFM_PARTITION_PLATFORM                 ON          CACHE BOOL      "Enable platform partition")
 set(MCUBOOT_DATA_SHARING                   ON          CACHE BOOL      "Enable Data Sharing")
 set(MCUBOOT_BOOTSTRAP                      ON          CACHE BOOL      "Allow initial state with images in secondary slots(empty primary slots)")
-set(MCUBOOT_ENC_IMAGES                     ON          CACHE BOOL      "Enable encrypted image upgrade support")
-set(MCUBOOT_ENCRYPT_RSA                    ON          CACHE BOOL      "Use RSA for encrypted image upgrade support")
+set(MCUBOOT_ENC_IMAGES                     OFF         CACHE BOOL      "Enable encrypted image upgrade support")
+# set(MCUBOOT_ENCRYPT_EC256                  ON          CACHE BOOL      "Use EC256 for encrypted image upgrade support")
 ################################## Dependencies ########################################
 set(TFM_PARTITION_INTERNAL_TRUSTED_STORAGE ON          CACHE BOOL      "Enable Internal Trusted Storage partition")
 set(TFM_PARTITION_CRYPTO                   ON          CACHE BOOL      "Enable Crypto partition")
