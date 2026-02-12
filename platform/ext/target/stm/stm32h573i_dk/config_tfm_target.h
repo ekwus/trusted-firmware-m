c0a3c93382dbc548e84563893f982e93fc133909
David Ritchie <d.ritchie@skillseeder.com>
2026-02-12 23:38:35 +0000

    Update the board configs to allow more ITS space

--- platform/ext/target/stm/stm32h573i_dk/config_tfm_target.h
+++ platform/ext/target/stm/stm32h573i_dk/config_tfm_target.h
@@ -15,4 +15,9 @@
 /* Use external RNG to provide entropy */
 #define CRYPTO_EXT_RNG                         1
 
+/* Increase ITS asset slots for Zephyr Settings persistence.
+ * Default is 10; we need ~8 UIDs for settings entries array,
+ * plus headroom for other ITS consumers (crypto keys, etc.). */
+#define ITS_NUM_ASSETS                         20
+
 #endif /* __CONFIG_TFM_TARGET_H__ */
